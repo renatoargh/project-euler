997e294769ed8490c5b3c01b010679337a538881
Renato Gama <renatoargh@gmail.com>
2013-11-06 18:06:20 -0200

    some refactorings

--- project-euler.js
+++ project-euler.js
@@ -1,9 +1,10 @@
 var problemId = process.argv[2],
-    problem = require('./' + problemId),
+    problem = require('./problems/' + problemId),
     title = 'Problem #' + problemId + ' - ' + problem.title;
 
+process.stdout.write('\u001B[2J\u001B[0;0f');
 console.log('~~~ Project Euler ~~~');
-console.log('---------------------');
+console.log();
 console.log(title + ': ');
 console.log(problem.description);
 console.log();
