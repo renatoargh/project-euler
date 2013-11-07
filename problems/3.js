1844fa734f43d27c658007c6cf64d959df7d3b8a
Renato Gama <renatoargh@gmail.com>
2013-11-07 03:15:17 -0200

    final improvement to problem #3

--- problems/3.js
+++ problems/3.js
@@ -5,25 +5,32 @@ module.exports.description =
     'The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime factor of the number 600851475143 ?';
 
 module.exports.solution = function largestPrimeFactor(){
-    var number = 600851475143, factor, biggestFactor = 0;
+    function getPrimeFactors(number){
+        var factors = [], factor;
 
-    function getPrimeFactor(number) {
-        for (var i = 2; i <= number; i++) {
-            if(number % i === 0) {
-                return i;
+        function getPrimeFactor(number) {
+            for (var i = 2; i <= Math.sqrt(number); i++) {
+                if(number % i === 0) {
+                    return i;
+                }
+            }
+
+            return number;
+        }
+
+        while(factor = getPrimeFactor(number)) {
+            factors.push(factor);
+            number /= factor;
+
+            if(number === 1) {
+                break;
             }
         }
 
-        return false;
-    }
+        return factors;
+    };
 
-    while((factor = getPrimeFactor(number))) {
-        if(factor > biggestFactor) {
-            biggestFactor = factor;
-        }
-
-        number = number / factor;
-    }
-
-    return biggestFactor;
+    return getPrimeFactors(600851475143).reduce(function(a, b){
+        return a > b ? a : b;
+    }, 0);
 };
