45db26cde3d4174f6bfba7a25245e8d1af8c8e64
Renato Gama <renatoargh@gmail.com>
2013-11-07 21:26:57 -0200

    showing solved problems summary and fixed wrong problemId for problem #7

--- project-euler.js
+++ project-euler.js
@@ -1,16 +1,40 @@
-var problemId = process.argv[2],
-    problem = require('./problems/' + problemId),
-    title = 'Problem #' + problemId + ' - ' + problem.title;
+var option = process.argv[2],
+    problem,
+    problemId,
+    title,
+    fs = require('fs');
 
 process.stdout.write('\u001B[2J\u001B[0;0f');
 console.log('~~~ Project Euler ~~~');
 console.log();
-console.log(title + ': ');
-console.log(problem.description);
-console.log();
-console.log('Solution:');
-console.log(problem.solution.toString());
-console.log();
-console.time('Execution Time');
-console.log('Answer: ' + problem.solution());
-console.timeEnd('Execution Time');
+
+if(!isNaN(option)) {
+    problemId = option;
+    problem = require('./problems/' + problemId),
+    title = 'Problem #' + problemId + ' - ' + problem.title;
+
+    console.log(title + ': ');
+    console.log(problem.description);
+    console.log();
+    console.log('Solution:');
+    console.log(problem.solution.toString());
+    console.log();
+    console.time('Execution Time');
+    console.log('Answer: ' + problem.solution());
+    console.timeEnd('Execution Time');
+}else if(option === 'solved') {
+    var problems = fs.readdirSync('./problems');
+    console.log('You have solved ' + problems.length + ' problems so far!');
+    console.log();
+    var a = problems.map(function(problem) {
+       return parseInt(problem.replace('.js', ''), 10);
+    }).sort(function(a, b){
+        return a - b;
+    }).forEach(function(problem) {
+        console.log('   ' + problem + ') ' + require('./problems/' + problem).title);
+    });
+
+    console.log();
+    console.log('Type the command "node project-euler #" where # is the problem you wanna see the solution');
+    console.log();
+}
