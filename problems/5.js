cf30d5c9c2ac9a4c2e146a58c1c711119e4576c1
Renato Gama <renatoargh@gmail.com>
2013-11-11 13:31:33 -0200

    minor fix

--- problems/5.js
+++ problems/5.js
@@ -10,7 +10,7 @@ module.exports.title =
 module.exports.description =
     '2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?';
 
-module.exports.solution = function largestPalindromeProduct(){
+module.exports.solution = function smallestMultiple(){
 
     var value = 20,
         answer = mathHelper.factorial(value),
